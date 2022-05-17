6281172d0089aba08be54e1a3d864ed426c1bd64
Janett Salzmann <Janett.Salzmann@t-systems.com>
2022-05-17 13:14:42 +0200

    TASK: Autogenerate schema types

--- src/server/database/sql/ingredients/create.sql
+++ src/server/database/sql/ingredients/create.sql
@@ -3,8 +3,10 @@
 */
 CREATE TABLE IF NOT EXISTS ingredients
 (
-    id serial PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name text NOT NULL,
     inventory_amount int DEFAULT 0,
     inventory_unit_id int references units(id),
+    created_at date DEFAULT NOW(),
+    updated_at date NOT NULL DEFAULT NOW()
 )
--- src/server/database/sql/units/create.sql
+++ src/server/database/sql/units/create.sql
@@ -1,8 +1,10 @@
 /*
     Creates table units.
 */
-CREATE TABLE units
+CREATE TABLE IF NOT EXISTS units
 (
     id serial PRIMARY KEY,
-    name text NOT NULL
+    name text NOT NULL,
+    created_at date DEFAULT NOW(),
+    updated_at date NOT NULL DEFAULT NOW()
 )
