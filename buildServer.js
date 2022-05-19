6db927ca57f6b6c9842132326c5dff75110e7518
Janett Salzmann <Janett.Salzmann@t-systems.com>
2022-05-19 14:04:49 +0200

    TASK: Buikd with esbuild

---
+++ buildServer.js
@@ -0,0 +1,19 @@
+require('esbuild').build({
+    entryPoints: ['./src/index.ts'],
+    bundle: true,
+    outfile: 'build/index.js',
+    tsconfig: 'tsconfig.server.json',
+    platform: 'node',
+    external: ['pg-native'],
+    plugins: [
+      require('esbuild-plugin-copy').copy({
+        assets: {
+          from: ['./src/database/sql/**/*.sql'],
+          // This is the oufile-directory of our esbuild config (see above)
+          to: ['./'],
+        },
+        keepStructure: true
+      })
+    ],
+  })
+  .catch(() => process.exit(1))
